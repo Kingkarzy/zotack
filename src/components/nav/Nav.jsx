0281b76ae59c28299d67c1069e89adfc090c357d
ciijay99 <chigozieahanwa99@gmail.com>
2023-03-20 19:41:42 +0100

    commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,61 +1,87 @@
 import { useState } from 'react';
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { links } from '../../data';
-
+import { NavLink } from 'react-router-dom';
+// import { Button } from './Button';
 import { BiMenu } from 'react-icons/bi';
 import { IoMdClose } from 'react-icons/io';
-// import {IoMdClose} from 'react-icons/io'
 import './nav.css';
 
-const Nav = () => {
-  const [isNavShowing, setIsNavShowing] = useState(false);
-  return (
-    <nav>
-      <div className='nav__container'>
-        <Link
-          to='/'
-          className='logo'
-          onClick={() => setIsNavShowing(false)}
-        >
-          <h3>Zotack</h3>
-        </Link>
-        <ul
-          className={`nav__links ${
-            isNavShowing ? 'show__nav' : 'hide__nav'
-          }`}
-        >
-          {links.map(({ name, path }, index) => {
-            return (
-              <li key={index}>
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? 'active-nav' : ''
-                  }
-                  onClick={() => setIsNavShowing((prev) => !prev)}
-                >
-                  {name}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-        <button
-          className='nav__toggle-btn'
-          onClick={() => setIsNavShowing((prev) => !prev)}
-        >
-          {isNavShowing ? <IoMdClose /> : <BiMenu />}
-        </button>
-      </div>
-    </nav>
-  );
-};
+function Nav() {
+  const [click, setClick] = useState(false);
+  // const [button, setButton] = useState(true);
 
-/* 
-window.addEventListener('scroll', () =>{
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 0)
-}); */
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  /* const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  window.addEventListener('resize', showButton);
+
+  useEffect(() => {
+    showButton();
+  }, []); */
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <NavLink
+            to='/'
+            className='navbar-logo'
+            onClick={closeMobileMenu}
+          >
+            <h3>Zotack</h3>
+          </NavLink>
+
+          <div
+            className='menu-icon'
+            onClick={handleClick}
+          >
+            {click ? (
+              <IoMdClose style={{ color: 'white' }} />
+            ) : (
+              <BiMenu style={{ color: 'white' }} />
+            )}
+          </div>
+
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <NavLink
+                to='/'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink
+                to='/About'
+                className='nav-links'
+                activeClassName='active'
+                onClick={closeMobileMenu}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink
+                to='/Contact'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 export default Nav;
